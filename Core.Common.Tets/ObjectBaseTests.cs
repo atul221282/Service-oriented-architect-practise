b8f96841ba0bf8e4f4b7562f2e4327742222e774
Atul Chaudhary <atul221282@gmail.com>
2016-06-24 23:35:04 +0930

    created test to test core objectbase class property change events and dirty checking and vaidation

---
+++ Core.Common.Tets/ObjectBaseTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Common.Tets
+{
+    [TestClass]
+    public class ObjectBaseTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
